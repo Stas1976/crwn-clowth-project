590e9d71f2095c1cab28f9cdf09382efe60b1685
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-07-14 12:45:10 +0300

    Created ShopPage and PreviewCollection components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
-import ShopPage from './pages/shop/Shop';
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
