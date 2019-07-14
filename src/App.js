e2a55fac7a3b15ff6ed19a531697468b950c8461
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-07-14 16:23:11 +0300

    Created SignIn form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
+import SignInSignOut from './pages/sign-in-and-sign-out/SigniInSignOut';
 import Header from './components/header/Header';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignOut} />
       </Switch>
     </div>
   );
