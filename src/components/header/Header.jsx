ca0bfe941848ca5140ae39ebaea0dbcba28c666c
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-07-19 18:41:03 +0300

    integreated firebase

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-const Header = () => {
+import { auth } from '../../firebase/firebase.utility';
+
+import './Header.scss';
+
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -15,6 +18,15 @@ const Header = () => {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="./signin">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
