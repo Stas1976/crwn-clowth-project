0858b4ee39978f3745a295dcbaa79a61a0781c70
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-07-19 13:37:58 +0300

    fixed authwithGoogle bug

--- src/App.js
+++ src/App.js
@@ -6,17 +6,42 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import SignInSignOut from './pages/sign-in-and-sign-out/SigniInSignOut';
 import Header from './components/header/Header';
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignOut} />
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utility';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignOut} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
