ee47ee2612d9b05c265f3bebc45401a5fc727102
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-07-23 16:49:29 +0300

    implemented SignIn functionality

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.utility';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utility';
 
 import './SignIn.scss';
 
@@ -16,9 +16,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
