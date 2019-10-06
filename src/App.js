0e444b456b940df1079fede7c3e59c80b54dfc39
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-10-06 15:49:34 +0300

    starting again e-shop

--- src/App.js
+++ src/App.js
@@ -2,54 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/homepage/HomePage';
-import ShopPage from './pages/shop/ShopPage';
-import SignInSignUp from './pages/sign-in-and-sign-up/SignInSignUp';
-import Header from './components/header/Header';
-import { auth, createUserProfileDocument } from './firebase/firebase.utility';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot,
-              ...snapShot.data()
-            }
-          });
-        });
-      }
-      this.setState({ currentUser: userAuth });
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInSignUp} />
-        </Switch>
+        <h3>App</h3>
       </div>
     );
   }
