1847f1c8b1926451cdb54639b3335030eaaf3aa1
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-10-20 19:35:17 +0300

    remove data to the firebase, before  functionality to delete

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Checkout from './pages/checkout/checkout.page';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utility';
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments
+} from './firebase/firebase.utility';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+import { selectCollectionForPreview } from './redux/shop/shop.selector';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -30,9 +35,13 @@ class App extends React.Component {
             ...snapShot.data()
           });
         });
-      } else {
-        setCurrentUser(userAuth);
       }
+
+      setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        'collections',
+        collectionArray.map(({ title, items }) => ({ title, items }))
+      );
     });
   }
 
@@ -62,7 +71,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionArray: selectCollectionForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
